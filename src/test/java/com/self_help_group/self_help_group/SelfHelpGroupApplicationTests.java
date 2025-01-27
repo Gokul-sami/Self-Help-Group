6283e9d88669725b9de8bd57742bc910dcc0ef6b
Gokul-sami <gokulpandiyan6@gmail.com>
2025-01-27 11:45:10 +0530

    Initial commit

---
+++ src/test/java/com/self_help_group/self_help_group/SelfHelpGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self_help_group.self_help_group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfHelpGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
