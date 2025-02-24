0b1eebe11be87a8932b5ab2ac51f4396727bfa6c
Gokul-sami <gokulpandiyan6@gmail.com>
2025-02-24 11:43:06 +0530

    routing to home

--- src/main/java/com/self_help_group/self_help_group/controller/LoginController.java
+++ src/main/java/com/self_help_group/self_help_group/controller/LoginController.java
@@ -3,14 +3,26 @@ package com.self_help_group.self_help_group.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import com.self_help_group.self_help_group.domain.Login;
 import com.self_help_group.self_help_group.service.LoginService;
+
+import ch.qos.logback.core.model.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class LoginController {
     @Autowired
     private LoginService serv;
-    @
-    public String showLoginPage(){
-
+    @GetMapping("/")
+    public String showLoginPage(@RequestParam String param) {
+        return "login";
+    }
+    @GetMapping("/login")
+    public String getMethodName(@RequestParam String username, @RequestParam String password, Model model) {
+        Login user = serv.log(username, password);
+        return "redirect:home";
     }
 }
